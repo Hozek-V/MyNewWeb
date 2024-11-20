76912c6f33b1cd447fc142c49785e01061d995b7
Hozek-V <janvystup@seznam.cz>
2024-11-20 20:38:24 +0100

    FilesVAdminu

--- pythonProject1/files/members/admin.py
+++ pythonProject1/files/members/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Member
+from .models import Member, File
 
 # Register your models here.
 
@@ -7,3 +7,9 @@ class MemberAdmin(admin.ModelAdmin):
   list_display = ("firstname", "lastname",)
 
 admin.site.register(Member, MemberAdmin)
+
+
+class FileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'stl_file', 'gcode_file', 'image', 'printer_name')
+
+admin.site.register(File, FileAdmin)
